50dbe924230b73c84d1d43a1dedd107de8d5b5c6
Felix Brandt <brandt.felix@outlook.com>
2016-05-19 11:16:38 +0200

    initial commit

---
+++ Assets/WalkingScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingScript : MonoBehaviour {
+
+    public Animator animator;
+    public float speed = 10f;
+
+    public int jumpHeight = 300;
+
+    private Rigidbody2D rb2d;
+    private bool jumping = false;
+
+    void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (!jumping)
+            {
+                animator.SetBool("Walking", true);
+            }
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+
+        // Jump
+        // TODO? only let player jump when standin on the ground
+        //       currently jumping after falling down is possible
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+        {
+            jumping = true;
+            animator.SetBool("Jumping", true);
+            //transform.Translate(Vector3.up * Time.deltaTime * jumpHeight);
+            rb2d.AddForce(new Vector2(0, jumpHeight));
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // TODO check if other object is a platform, only reset then.
+        jumping = false;
+        animator.SetBool("Jumping", false);
+    }
+
+}
