01ae394c942c7d227f74dc80099a69f41b81534a
dfraas <davidfraas@eb.de>
2016-05-27 15:58:46 +0200

    Item HUD! (ﾉ◕ヮ◕)ﾉ

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ItemController : MonoBehaviour {
@@ -6,11 +7,19 @@ public class ItemController : MonoBehaviour {
 	public GameObject[] weapons = new GameObject[2];
 	public GameObject[] tools = new GameObject[2];
     public float damage = 0f;
+    private Button weaponSprite1;
+    private Button weaponSprite2;
+    private Button toolSprite1;
+    private Button toolSprite2;
 
 
-	// Use this for initialization
-	void Start () {
-
+    // Use this for initialization
+    void Start() {
+        weaponSprite1 = GameObject.Find("HUD/Items/WeaponSlots").GetComponentsInChildren<Button>()[0];
+        weaponSprite2 = GameObject.Find("HUD/Items/WeaponSlots").GetComponentsInChildren<Button>()[1];
+        toolSprite1 = GameObject.Find("HUD/Items/ToolSlots").GetComponentsInChildren<Button>()[0];
+        toolSprite2 = GameObject.Find("HUD/Items/ToolSlots").GetComponentsInChildren<Button>()[1];
+            
 	}
 	
 	// Update is called once per frame
@@ -26,7 +35,20 @@ public class ItemController : MonoBehaviour {
                 switch (collider.gameObject.name)
                 {
                     case "KnifeItem":
-                        transform.Find("player_torso/player_arm_upper/player_arm_lower/player_hand/Knife").gameObject.SetActive(true);
+                        weapons[0] = transform.Find("player_torso/player_arm_upper/player_arm_lower/player_hand/Knife").gameObject;
+                        weapons[0].SetActive(true);
+                        Sprite sprite = Resources.Load<Sprite>("Weapons/Knife");
+                        if (sprite != null)
+                        {
+                            weaponSprite1.image.overrideSprite = sprite;
+                            weaponSprite1.image.color = Color.white;            // empty slot has Alpha(opacity) to 0, giving an image the color white simply sets the opacity to 100
+                        }
+                        else
+                        {
+                            Debug.Log("Sprite not found");
+                        }
+                        
+
                         break;
                         //case "Taser":
                         //case "Revolver":
