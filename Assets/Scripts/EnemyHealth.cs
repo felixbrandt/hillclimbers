84695bbd3275a851d14aa739980e4f02d4a84a33
Manuel Schreiber <manuel.schreiber@hof-university.de>
2016-05-30 21:09:03 +0200

    added some enemy scripts
    check collusions between walking script an enemy script. different
    enemy walking script available

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public float health=100f;
+	public float maxHealth=100f;
+
+	public void RemoveHealth(float amount)
+	{
+		//Check if Enemy has enough Health left to get fought
+		if (health > 0) {
+			health -= amount;
+		}
+		//Kill enemy if health <= 0
+		else {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	//TODO Adapt script to Enemy 
+	public float playerDamage;
+
+	Animator animator;
+	Transform target;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//TODO write base script
+	private void AttemptMove<T>(int xDir,int yDir)
+	{
+		base.AttemptMove<T> (xDir, yDir);
+	}
+	public void MoveEnemy()
+	{
+		int xDir = 0;
+		int yDir = 0;
+		if (Mathf.Abs (target.position.x - transform.position.x) < float.Epsilon) {
+			yDir = target.position.x > transform.position.y ? 1 : -1;
+		} else {
+			xDir = target.position.x > transform.position.x ? 1 : -1;
+		}
+		AttemptMove<Player> (xDir, yDir);
+	}
+}
