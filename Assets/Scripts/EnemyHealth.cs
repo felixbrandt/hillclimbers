74550e6eac84c3c4874b87c6d90295d8ab9399ca
Felix Brandt <brandt.felix@outlook.com>
2016-06-01 13:53:23 +0200

    revert "fick dich david"
    This reverts commit 546ecc6ebe6706228a38994139bc2801b6d783c7.

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EnemyHealth : MonoBehaviour {
 
 	public float health=100f;
+	public float maxHealth=100f;
 
 	public void RemoveHealth(float amount)
 	{
@@ -13,7 +14,6 @@ public class EnemyHealth : MonoBehaviour {
 		}
 		//Kill enemy if health <= 0
 		else {
-			//TODO Show dying-Animation
 			Destroy (gameObject);
 		}
 	}
